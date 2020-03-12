b06057dc20e773d61d0a6d50ff124e242ea4d5fa
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2020-03-12 08:17:55 +0000

    Show a message if there are no list items

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,43 @@
 
   "use strict";
 
+  //
+  // Variables
+  //
+
+  // The Reef instance for the component
+  var app = new Reef("#app", {
+    data: { listItems: [] },
+    template: template
+  });
+
+
+  //
+  // Functions
+  //
+
+  /**
+   * Return the markup for the UI based on the current state
+   * @param {Object} props The state data
+   */
+  function template (props) {
+
+    // If there are no list items, show a message
+    if (props.listItems.length < 1) {
+      return "<p>You haven't added any list items yet. Why not start now?</p>";
+    }
+
+  }
+
+
+  //
+  // Init
+  //
+
+  // Log the Reef instance to the console
+  console.log(app);
+
+  // Render the component
+  app.render();
+
 })();
