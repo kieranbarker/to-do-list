24468d47507f9360aa2319455f4f8c31b0b4fe6a
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2020-03-16 00:03:14 +0000

    Add helper function for getting URL params

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,33 @@
   //
 
   /**
+   * Get the URL parameters
+   * source: https://css-tricks.com/snippets/javascript/get-url-variables/
+   * @param  {String} url The URL
+   * @return {Object}     The URL parameters
+   */
+  function getParams (url) {
+
+    var params = {};
+    var parser = document.createElement("a");
+
+    parser.href = url ? url : window.location.href;
+
+    var query = parser.search.substring(1);
+    var vars = query.split("&");
+
+    if (vars.length < 1 || vars[0].length < 1) return params;
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    }
+
+    return params;
+
+  }
+
+  /**
    * Return the markup for the UI based on the current state
    * @param  {Object} props The state data
    * @return {String}       An HTML string
